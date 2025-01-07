eb09e33d8943195aa980bb06c0dba3e53adbb0cc
Rueben <akankobatengr@gmail.com>
2025-01-07 22:53:16 +0000

    Initial Commit

---
+++ PayX/PayXApp.swift
@@ -0,0 +1,17 @@
+//
+//  PayXApp.swift
+//  PayX
+//
+//  Created by Rueben on 07/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct PayXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
